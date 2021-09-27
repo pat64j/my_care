f61b8e07defd3bcb937dcb59db5210428e51a69d
Patrick Adonteng <pat64j@live.com>
2021-09-27 15:57:31 +0000

    implement dashboard design

--- lib/src/views/home/home_dash.dart
+++ lib/src/views/home/home_dash.dart
@@ -6,52 +6,68 @@ class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Card(
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)
-                      ),
-                      child: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: const [
-                            FlutterLogo(),
-                            Text('data')
-                          ],
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8)
-                      ),
-                      child: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: const [
-                            FlutterLogo(),
-                            Text('data')
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
+      body: Stack(
+        children: [dashBg, content],
       ),
     );
   }
+
+  get dashBg => Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(color: Colors.deepPurple),
+            flex: 2,
+          ),
+          Expanded(
+            child: Container(color: Colors.transparent),
+            flex: 5,
+          ),
+        ],
+      );
+
+  get content => SafeArea(
+        child: Column(
+          children: <Widget>[
+            header,
+            grid,
+          ],
+        ),
+      );
+
+  get header => const ListTile(
+        contentPadding: EdgeInsets.only(left: 20, right: 20, top: 20),
+        title: Text(
+          'Dashboard',
+          style: TextStyle(color: Colors.white),
+        ),
+        subtitle: Text(
+          '10 items',
+          style: TextStyle(color: Colors.blue),
+        ),
+        trailing: CircleAvatar(),
+      );
+
+  get grid => Expanded(
+        child: Container(
+          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+          child: GridView.count(
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            crossAxisCount: 2,
+            childAspectRatio: .90,
+            children: List.generate(6, (_) {
+              return Card(
+                elevation: 2,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [FlutterLogo(), Text('data')],
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+      );
 }
